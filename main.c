9190624b04e550230e6324b6b2c28b81b815b7c2
Filip <fkaplunow@gmail.com>
2021-12-30 20:25:36 +0100

    TPM configured

--- main.c
+++ main.c
@@ -1,19 +1,21 @@
 #include "lcd1602.h"
 #include "buttons.h"
+#include "tpm.h"
 #include <stdio.h>
 
-void LED_Init() {
-	SIM->SCGC5 |=  SIM_SCGC5_PORTB_MASK;
-	PORTB->PCR[9] |= PORT_PCR_MUX(1);
-	FPTB->PDDR |= (1 << 9);
-	FPTB->PSOR |= (1 << 9);
-}
+//void LED_Init() {
+//	SIM->SCGC5 |=  SIM_SCGC5_PORTB_MASK;
+//	PORTB->PCR[9] |= PORT_PCR_MUX(1);
+//	FPTB->PDDR |= (1 << 9);
+//	FPTB->PSOR |= (1 << 9);
+//}
 
 int main()
 {
 	buttons_Init();
 	LCD1602_Init();
-	LED_Init();
+	TPM0_Init();
+	//LED_Init();
 	while (1)
 	{
 		char str[50];
---
+++ tpm.h
@@ -0,0 +1,8 @@
+#ifndef TPM_H
+#define TPM_H
+
+#include "MKL05Z4.h"
+
+void TPM0_Init(void);
+
+#endif  /* TPM_H */
